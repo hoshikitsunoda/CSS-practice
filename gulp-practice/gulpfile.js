734432e580d1da7d4f4b1426d8a07aa98971a2d7
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-08 16:24:30 -0800

    sass task added.

--- gulp-practice/gulpfile.js
+++ gulp-practice/gulpfile.js
@@ -2,8 +2,14 @@ var gulp = require('gulp')
 var sass = require('gulp-sass')
 var connect = require('gulp-connect')
 
-gulp.task('hello-world', function(){
-    console.log('hello world')
+gulp.task('hello-world', function() {
+  console.log('hello world')
+})
+
+gulp.task('sass', function() {
+  return gulp.src('source-files')
+    .pipe(sass())
+    .pipe(gulp.dest('destination'))
 })
 
 gulp.task('connect', function() {
