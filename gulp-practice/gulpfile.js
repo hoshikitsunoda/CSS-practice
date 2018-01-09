219087ae8f837ea945d45d2a3f63ab6135bad482
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-08 17:51:21 -0800

    browserSync task added.

--- gulp-practice/gulpfile.js
+++ gulp-practice/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
+var browserSync = require('browser-sync').create()
 var connect = require('gulp-connect')
 
 gulp.task('hello-world', function() {
@@ -10,10 +11,21 @@ gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 })
 
 gulp.task('watch', function() {
   gulp.watch('app/scss/**/*.scss', ['sass'])
+})
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
 })
 
 gulp.task('connect', function() {
