00b1ddd3867ff6eea93fe5643cc33998f1ac437f
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-09 23:48:15 -0800

    gulp tasks added.

--- bourbon-neat-practice/Gulpfile.js
+++ bourbon-neat-practice/Gulpfile.js
@@ -1,20 +1,37 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    bourbon = require('bourbon').includePaths,
-    neat = require('node-neat').includePaths
+    bourbon = require('node-bourbon').includePaths,
+    neat = require('node-neat').includePaths,
+    browserSync = require('browser-sync').create()
 
 var paths = {
   scss: './assets/sass/*.scss'
 }
 
-gulp.task('styles', function () {
+gulp.task('sass', function() {
   return gulp.src(paths.scss)
     .pipe(sass({
       includePaths: [bourbon, neat]
     }))
     .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 })
 
-gulp.task('default',function(){
-  gulp.start('styles')
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'public'
+    },
+  })
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+  gulp.watch('./assets/sass/*.scss', ['sass'])
+  gulp.watch('public/*.html', browserSync.reload)
+})
+
+gulp.task('default',function(callback){
+  gulp.start(['sass', 'browserSync', 'watch'])
 })
