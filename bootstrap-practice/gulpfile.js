6dfef07f3758b2fadfc996db06badd4d5f9f2f93
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-10 16:41:55 -0800

    gulp setup to auto reload on change on html.

---
+++ bootstrap-practice/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass')
+    browserSync = require('browser-sync').create()
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    },
+  })
+})
+
+gulp.task('watch', ['browserSync'], function() {
+  gulp.watch('dist/*.html', browserSync.reload)
+})
+
+gulp.task('default',function(callback){
+  gulp.start(['browserSync', 'watch'])
+})
